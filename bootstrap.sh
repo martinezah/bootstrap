2d1f9b4bb0ecbd56f7409c77c43e75931ee78b73
Marti Martinez <martinezah@gmail.com>
2015-06-13 09:25:24 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
-sudo apt-get install -y ansible git
-git clone git@github.com:martinezah/bootstrap.git
-ansible-playbook -i bootstrap/inventory bootstrap/bootstrap.yml
+sudo apt-get install -y ansible git &&
+git clone git@github.com:martinezah/bootstrap.git &&
+ansible-playbook -i bootstrap/inventory bootstrap/bootstrap.yml &&
+rm -rf bootstrap
